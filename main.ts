5d15551d8875d918980ac91168347a85efa807b1
TristenWilliam <108334238+TristenWilliam@users.noreply.github.com>
2022-07-14 09:43:30 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+    basic.showNumber(999 * 43.624 % Math.sqrt(34.5))
+})
